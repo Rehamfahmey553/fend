436ec6088a291625974f48f5c53a8c830ecddb44
Rehamfahmey553 <122476575+Rehamfahmey553@users.noreply.github.com>
2023-01-12 21:39:12 +0200

    Update server.js

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -1,9 +1,15 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+projectData = {weather -app. map};
 
 // Require Express to run server and routes
-
+constexpress=require('express');
+constapp=express();
+//GET DATAF ROM SERVER ROUTE
 // Start up an instance of app
+app.get("/getdata,function(req,res){ });
+        //postDATAto SERVER ROUTE
+        app.post("/postdata",function(req,res){};
+constport=6666;// CREATE SERVER.
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -15,5 +21,5 @@ app.use(bodyParser.json());
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
+app.listen(port,()>{console.log('server is running on port ${port}')
 // Setup Server
