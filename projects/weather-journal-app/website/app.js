8794963a013ff315eebc2500d8320f025f9c7e5b
Rehamfahmey553 <122476575+Rehamfahmey553@users.noreply.github.com>
2023-01-12 22:39:09 +0200

    Update app.js

--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -1,4 +1,16 @@
 /* Global Variables */
+//create3requests;
+//GET REQUEST=>OPEN WEATHEr MAP API
+constGetData=async()=>{try{constrequest=awaitgetch(: ")//jsonformat
+                                                   constresponse=awaitrequest.Josn()
+                           consttemp=response.main.tempreturn temp{ catch(err){console.log(err)}}
+                           // 2postrequest=>localserver
+                           const server = app.listen(port, ()=>{console.log(`running on localhost: ${port}`)})
+                           function listening(){
+     console.log("server running"); 
+     console.log(`running on localhost: {$port}`);
+}
+                           
 
 // Create a new date instance dynamically with JS
 let d = new Date();
